c0a2b84d107fc0350ca78f83d8cfdef4ae14fb84
Dima <evtushenko.2002@gmail.com>
2017-04-01 12:39:40 +0300

    F*ck yea, it's finaly works!

--- src/isLive.c
+++ src/isLive.c
@@ -6,6 +6,6 @@ int isLive(int x)
   if(right==-1) right=l-1;
   int left=(x+1)%l;
   int state=map[right]*4+map[x]*2+map[left];
-  rule=rule>>state;
-  if(rule%2) return 1;
+  int newrule=rule>>state;
+  if(newrule%2) return 1;
 }
--- src/updateMap.c
+++ src/updateMap.c
@@ -2,8 +2,12 @@ int* map;int rule;int l;
 
 void updateMap()
 {
+  int* newmap=(int*)malloc(sizeof(int)*l);
   for(int i=0;i<l;i++)
   {
-    map[i]=isLive(i);
+    newmap[i]=isLive(i);
   }
+  for(int i=0;i<l;i++)
+    map[i]=newmap[i];
+  free(newmap);
 }
